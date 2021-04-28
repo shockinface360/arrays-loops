380f13e951e40c54922fe3db0846d5afe01ed5f7
M@RK <manfredi23251@lcsd2kids.org>
2021-04-28 11:25:08 -0600

    learing again

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hi")
+var shoppingList = ["Eggs","Pickles","Ham","Milk"];
+
+const container = document.querySelector('#container');
+let newElement = document.createElement("h1");
+
+
+
+for (let index = 0; index < 10; index++) {
+
+    newElement = document.createElement("h1");
+    newElement.textContent = "new stuff here" +index;
+    container.appendChild(newElement);
+
+}
+
+
+// var lunchBox ={
+// drink: "Water",
+// sandwich: "Ham",
+// desert:"Oreos",
+// chips:"Sun-chips",
+// veggies:"brocolli"
+// };
+// document.write(lunchBox.drink)
